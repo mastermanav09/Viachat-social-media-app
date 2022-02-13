ef66d792b2bdbfa2ae2e0d9a9e3f07c58cd17710
Manav Naharwal <manavnaharwalp@gmail.com>
2022-02-13 20:49:12 +0530

    Add socket server configuration.

---
+++ config/socket.js
@@ -0,0 +1,22 @@
+let io;
+
+module.exports = {
+  init: (httpServer) => {
+    io = require("socket.io")(httpServer, {
+      cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+      },
+    });
+    return io;
+  },
+
+  getIO: () => {
+    if (!io) {
+      console.log("Socket.io not initialized");
+      throw new Error("Couldn't connect with the server.");
+    }
+
+    return io;
+  },
+};
