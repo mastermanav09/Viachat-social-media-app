dafc329bb8fe60f29e8fc0c90749a91801bde53e
Manav Naharwal <manavnaharwalp@gmail.com>
2022-02-13 20:37:48 +0530

    Add profile validation

---
+++ utils/validators/profile/profleValidation.js
@@ -0,0 +1,26 @@
+const { body } = require("express-validator");
+
+const profileValidationArray = [
+  body("age").custom((value, { req }) => {
+    if (value && (NaN(value) || value < 12 || value > 120)) {
+      return Promise.reject("Please enter your valid age!.");
+    }
+
+    return true;
+  }),
+
+  body("website").custom((value, { req }) => {
+    if (value) {
+      let siteReg =
+        /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/;
+
+      if (!siteReg.test(value)) {
+        return Promise.reject("Please enter a valid website.");
+      }
+    }
+
+    return true;
+  }),
+];
+
+module.exports = profileValidationArray;
